aa3c83ccfbc4bbd6461650dd491e44fa0b5af6b4
Edwin Cloud <ecloud412@gmail.com>
2018-11-22 10:34:07 -0800

    [add] Can now guess numbers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 
 fn main() {
   println!("Hello rusty world!");
 
-  println!("Please input your favorite number: ");
+  // generate a random number
+  let rand_num = rand::thread_rng().gen_range(1, 101);
 
-  let mut fav_num = String::new();
+  // loop until correct number guessed
+  loop {
+    println!("Please guess a number between 1 and 100: ");
 
-  io::stdin().read_line(&mut fav_num)
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
       .expect("failed to read line womp");
 
-  println!("No way... {} is my favorite number too!", fav_num);
+    // convert guess to number or ask again
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+
+    // let user know how close they are
+    match guess.cmp(&rand_num) {
+      Ordering::Less => println!("A bit higher.."),
+      Ordering::Greater => println!("Too much!!"),
+      Ordering::Equal => {
+        println!("You guessed it!! {}", guess);
+        break;
+      }
+    }
+  }
 }
