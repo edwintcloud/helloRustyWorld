ffc51658727bdb27d5363f5e18d93678a3f14044
Edwin Cloud <ecloud412@gmail.com>
2018-11-22 00:36:26 -0800

    [add] Added number input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
   println!("Hello rusty world!");
+
+  println!("Please input your favorite number: ");
+
+  let mut fav_num = String::new();
+
+  io::stdin().read_line(&mut fav_num)
+      .expect("failed to read line womp");
+
+  println!("No way... {} is my favorite number too!", fav_num);
 }
